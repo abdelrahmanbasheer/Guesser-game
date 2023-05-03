2757de5bcc38d37299d4e055b80bc872f422138e
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2023-05-03 04:21:18 +0200

    b

--- utils/function.js
+++ utils/function.js
@@ -2,7 +2,7 @@ import  useSWR  from "swr"
 
 export function getPlayer(id){
     const fetcher = (...args) => fetch(...args).then((res) => res.json())
-    const {data,isLoading,error}=useSWR(`http://localhost:3000/api/players/${id}`,fetcher,{revalidateOnMount:false,revalidateOnFocus:false,revalidateIfStale:false,})
+    const {data,isLoading,error}=useSWR(`/api/players/${id}`,fetcher,{revalidateOnMount:false,revalidateOnFocus:false,revalidateIfStale:false,})
 
     return {
         data: data,
@@ -13,7 +13,7 @@ export function getPlayer(id){
 }
 export function getAllPlayers(){
     const fetcher = (...args) => fetch(...args).then((res) => res.json())
-    const {data,error,isLoading}=useSWR('http://localhost:3000/api/players',fetcher)
+    const {data,error,isLoading}=useSWR('/api/players',fetcher)
     return {
         data: data,
         isLoading,
