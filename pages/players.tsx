59b326300706848fbbd1f2af0eb9c71f928856b8
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2023-05-03 04:42:20 +0200

    b

--- pages/players.tsx
+++ pages/players.tsx
@@ -3,21 +3,10 @@ import React from 'react'
 import PlayerCard from '@/components/PlayerCard'
 import Link from 'next/link'
 import { getAllPlayers } from '@/utils/function'
+import {  PrismaClient } from '@prisma/client'
 
-const players = () => {
-  
-    const {data,isLoading,isError}=getAllPlayers()
-    
-    if(isLoading) return(
-        <div className='h-screen bg-black'>
-        <img src="https://e0.365dm.com/22/05/2048x1152/skysports-graphic-player-premier_5770435.jpg" alt="football players loading image" />
-        </div>
-    )
-    if(isError)return(
-        <h1>this is an error{isError}</h1>
-    )
-    
-  if(data)return (
+const players = ({data}) => {
+  return (
     <div className='bg-black h-full pb-10'>
         <h1 className='text-3xl text-white font-bold mb-5 p-4'>Players:</h1>
         <ul className='flex md:flex-row justify-center flex-wrap flex-col m-10 gap-5'>
@@ -39,3 +28,12 @@ const players = () => {
 }
 
 export default players
+export async function getStaticProps() {
+    const prisma = new PrismaClient()
+    const data=await prisma.player.findMany()
+    
+    
+    return{
+        props:{data}
+    }
+}
