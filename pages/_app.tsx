e011cc1ddabd6346ce287eb3b39399d20be6e66b
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2023-05-04 06:39:19 +0200

    b

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }: AppProps) {
   return(
   <>
   <Navbar></Navbar>
   <Component {...pageProps} />
+  <Analytics />
   </>
   )
 }
